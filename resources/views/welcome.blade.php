f07508555c7aafa0ced5a9dbc056ccec381ca0b6
yusufftt <syaifuly482@gmail.com>
2024-02-28 15:43:50 +1400

    warna perkenalan diri
    diubah dari hitam menjadi biru

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Perkenalan Diri</title>
+        <style>
+        /* Menggunakan CSS untuk mengubah warna tulisan menjadi biru */
+        title {
+            color: blue;
+        }
+        </style>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
